a9094179e8d9b2203b656747a653ce6d54530116
tosi29 <tosi16@gmail.com>
2020-11-17 22:10:36 +0900

    Initial commit

---
+++ src/test/java/com/example/spocktestsample/SpockTestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spocktestsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockTestSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
